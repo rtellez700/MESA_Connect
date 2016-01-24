81953a76398831b06fa5a1661b5c5b9c21d2a78c
David.Ly <dly5891@gmail.com>
2016-01-24 13:27:55 -0600

    Finishing up environment initialization

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-'use strict'
+'use strict';
 
 var gulp = require('gulp');
 var connect = require('gulp-connect'); //Runs a local dev server
 var open = require('gulp-open'); //Open a URL in a web browser
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
+
 
 var config = {
   port: 8001,
   devBaseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
+    js: './src/**/*.js',
+    css: [
+      'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    ],
+    mainJS: './src/main.js',
     dist: './dist',
   }
 };
@@ -31,11 +43,34 @@ gulp.task('open', ['connect'], function(){
 gulp.task('html', function(){
   gulp.src(config.paths.html)
     .pipe(gulp.dest(config.paths.dist))
-    .pipe(connect.reload())
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function(){
+  browserify(config.paths.mainJS)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+  gulp.src(config.paths.css)
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest(config.paths.dist+'/css'));
+});
+
+gulp.task('lint', function(){
+  return gulp.src(config.paths.js)
+          .pipe(lint({config: '.eslintrc'}))
+          .pipe(lint.format());
 });
 
 gulp.task('watch', function(){
   gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+$ = jQuery = require('jquery');
+var App = console.log('Hello World from Browserify');
+module.exports = App;
